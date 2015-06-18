8c45b7c199f56940216600d81bad4bf899641481
jutta-frieden <jutta.frieden@gmail.com>
2015-06-18 10:22:39 +0100

    made German CL job advert more salesy

--- app/pages/about/jobs/positions/country-lead-de-at.js
+++ app/pages/about/jobs/positions/country-lead-de-at.js
@@ -27,11 +27,19 @@ export default class CountryLeadGermanyAustria extends React.Component {
           The role
         </h4>
         <p className='para'>
-          We've laid foundations in the UK, and these will
+          We've laid strong foundations in the UK, and these will
           guide you as you figure out a strategy that will deliver rapid growth
           in Germany and Austria. Then you'll execute this strategy, driving in-country
           work independently and resourcefully, and swiftly becoming an expert in your
           market.
+        </p>
+         <p className='para'>
+          You have been taught that one part of being an entrepreneur is to be a salesperson?
+          In this role, you will put this into practice. All client-facing activities in your
+          market will be your responsibility: planning and delivering on launch activities to
+          build momentum early and accompanying prospective customers from sales to live.
+          You'll handle initial marketing, PR, sales, partnerships and account management,
+          and start to build a team to support &amp; scale growth in your market.
         </p>
         <p className='para'>
           You will work closely with leaders across the company to understand all product,
@@ -40,17 +48,14 @@ export default class CountryLeadGermanyAustria extends React.Component {
           feed your market's needs into our development roadmap to ensure we're always
           building the product people want.
         </p>
-        <p className='para'>
-          All client-facing activities in your market will be your responsibility: planning
-          and delivering on launch activities to build momentum early; accompanying
-          prospective customers from sales to live. You'll handle initial marketing,
-          PR, sales, partnerships and account management, and start to build a team
-          to support &amp; scale growth in your market.
-        </p>
+       
         <p className='para'>
           Launching GoCardless in a new territory will require a depth and breadth of
           skills akin to starting a new company. It's an exciting challenge, and you'll
           benefit from the support and experience of our existing country leads.
+          In fact, we believe that launching GoCardless in a new territory as important
+          as Germany and Austria requires two of your kind, to keep you on your toes and offer
+          an opportunity to discuss ideas and strategies, but also to challenge each other.
         </p>
         <h4 className='u-text-s u-margin-Bxs u-margin-Tm'>
           The ideal candidate
