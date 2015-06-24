5c2aa8a82c52b8f461dd81727b0194f6f0bb6ee6
Sam Jewell <sam.jewell@cantab.net>
2015-06-24 11:41:22 +0100

    fixing broken nav

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -78,14 +78,14 @@ class Header extends React.Component {
                    {products}
                  </Popover>
               </div>
-              <IfLinkExists to='pricing' className='nav__item u-relative'>
+              <IfLinkExists to='pricing' tagName='div' className='nav__item u-relative'>
                 <Link to='pricing' id='track-nav-pricing' className={linkClass}>
                   <div className='nav__item-link'>
                     <Message pointer='pricing.nav_title' />
                   </div>
                 </Link>
               </IfLinkExists>
-              <IfLinkExists to='stories' className='nav__item u-relative'>
+              <IfLinkExists to='stories' tagName='div' className='nav__item u-relative'>
                 <Link to='stories' id='track-nav-stories' className={linkClass}>
                   <div className='nav__item-link'>
                     <Message pointer='stories.nav_title' />
