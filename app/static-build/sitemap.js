0ade18367b79c1e0078c0fc6a750d95562985a94
Ken-Lo <ken@kenstechtips.com>
2015-07-27 18:48:08 +0100

    Change all vars to let/const

--- app/static-build/sitemap.js
+++ app/static-build/sitemap.js
@@ -11,15 +11,15 @@ const crypto = require('crypto');
  */
 function getHashOfContent(content) {
   content = content.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '');
-  var $ = cheerio.load(content);
-  var bodyText = $('body').text();
-  var cryptoHash = crypto.createHash('md5').update(bodyText).digest('hex');
+  const $ = cheerio.load(content);
+  const bodyText = $('body').text();
+  const cryptoHash = crypto.createHash('md5').update(bodyText).digest('hex');
   return cryptoHash;
 }
 
 /**
  * Creates a new SitemapUrlSet object.
- * e.g. var sitemap = new SitemapUrlSet('http://example.com')
+ * e.g. let sitemap = new SitemapUrlSet('http://example.com')
  *
  * @class
  * @param {string} domain - Domain to which the sitemap refers e.g. https://example.com
@@ -32,21 +32,21 @@ export class SitemapUrlSet {
 
   /**
    * Loads in an existing XML Sitemap and returns a SitemapUrlSet object.
-   * e.g. var currentSitemap = new SitemapUrlSet('http://example.com').fromXml(xmlFileContents)
+   * e.g. let currentSitemap = new SitemapUrlSet('http://example.com').fromXml(xmlFileContents)
    *
    * @param {string} xml - Sitemap in XML format
    * @returns {SitemapUrlSet} A SitemapUrlSet object representing the sitemap.
    */
   fromXml(xml) {
-    var $ = cheerio.load(xml);
-    var sitemapUrls = $('url');
+    const $ = cheerio.load(xml);
+    const sitemapUrls = $('url');
     sitemapUrls.each((i, element) => {
       let currentUrlElement = $(element);
-      var location = currentUrlElement.find('loc').text();
+      let location = currentUrlElement.find('loc').text();
       if (location) {
-        var pathName = url.parse(location).pathname;
-        var hash = currentUrlElement.find('content\\:hash').text();
-        var lastMod = Date.parse(currentUrlElement.find('lastmod').text());
+        let pathName = url.parse(location).pathname;
+        let hash = currentUrlElement.find('content\\:hash').text();
+        let lastMod = Date.parse(currentUrlElement.find('lastmod').text());
         this.urlSet[pathName] = {
           hash: hash,
           lastMod: lastMod,
@@ -63,7 +63,7 @@ export class SitemapUrlSet {
    */
   toXml() {
     /*eslint-disable max-len*/
-    var xml = '<?xml version="1.0" encoding="UTF-8"?>\n' +
+    let xml = '<?xml version="1.0" encoding="UTF-8"?>\n' +
               '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n' +
               '  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"\n' +
               '  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"\n' +
@@ -71,9 +71,9 @@ export class SitemapUrlSet {
               '  xmlns:content="https://github.com/gocardless/splash-pages#xml-sitemap-hash">\n';
     /*eslint-enable max-len*/
     for (let pageUrl in this.urlSet) {
-      var lastMod = new Date(this.urlSet[pageUrl].lastMod);
+      let lastMod = new Date(this.urlSet[pageUrl].lastMod);
       // Date should be in W3C Datetime (YYYY-MM-DD) format
-      var lastModW3 = lastMod.toISOString().substring(0,10);
+      let lastModW3 = lastMod.toISOString().substring(0,10);
       xml += `  <url>\n` +
              `    <loc>${this.domain}${pageUrl}</loc>\n` +
              `    <content:hash>${this.urlSet[pageUrl].hash}</content:hash>\n` +
@@ -102,7 +102,7 @@ export class SitemapUrlSet {
 
   /**
    * Extracts a URL from the sitemap.
-   * e.g. var aboutPage = sitemap.getUrl('http://www.example.com/about/')
+   * e.g. const aboutPage = sitemap.getUrl('http://www.example.com/about/')
    *
    * @param {string} pageUrl - URL of the page to add to the sitemap
    * @return {{Object|boolean}} Returns URL info if exists in the sitemap; otherwise returns false
@@ -141,8 +141,8 @@ export class SitemapUrlSet {
   importTimestampsFromOldSitemap(oldSitemap) {
     for (url in this.urlSet) {
       if (url in oldSitemap.urlSet) {
-        var oldUrl = oldSitemap.urlSet[url];
-        var newUrl = this.urlSet[url];
+        let oldUrl = oldSitemap.urlSet[url];
+        let newUrl = this.urlSet[url];
         if (oldUrl.hash === newUrl.hash) {
           newUrl.lastMod = Math.min(newUrl.lastMod, oldUrl.lastMod);
         }
