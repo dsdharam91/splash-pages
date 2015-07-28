262cf35f3c60c466088068a6a41b512e9f7c6838
Ken-Lo <ken@kenstechtips.com>
2015-07-28 17:08:30 +0100

    Update English Tutorials on Payment & Subscription Creation

--- app/pages/faq/pro-dashboard/payment-creation/faq-pro-dashboard-payment-creation.en.js
+++ app/pages/faq/pro-dashboard/payment-creation/faq-pro-dashboard-payment-creation.en.js
@@ -10,24 +10,50 @@ export default class FaqProDashboardPaymentCreationEn extends React.Component {
       <Translation locales='en'>
         <h2 className='u-text-heading-light u-text-m u-color-heading'>Payment creation</h2>
         <p className='para'>
-          If you wish to take payments with varying amounts or unfrequent frequency, create individual payments.
-          Otherwise please take a look at the <Link to='faq_pro_dashboard_subscription_creation'
-          className='u-link-color-p u-text-underline'>Subscription section</Link>.
+          After setting up your customer, you'll be able to start taking Direct Debit payments from them. On
+          the <a href='https://manage.gocardless.com/customers' 
+          target='_blank' className='u-link-color-p u-text-underline'>customers section</a> of the dashboard, choose the customer
+          from whom you want to take payments.
         </p>
+        <ul className='list'>
+          <li>
+            <strong>If you're taking payments of a varying amount or varying frequency</strong>, you should set up an individual payment.
+            This tutorial shows you how to create individual payments.
+          </li>
+          <li>
+            <strong>If you're taking payments of a regular amount at a regular frequency</strong>, you should set up a subscription
+            for the customer. For more information, see our <Link to='faq_pro_dashboard_subscription_creation'
+            className='u-link-color-p u-text-underline'>tutorial on subscription creation</Link>.
+          </li>
+        </ul>
+
+        <h3 className='section-heading u-text-heading-light u-color-heading u-margin-Vm u-text-s'>
+          Creating a Payment for your Customer
+        </h3>
         <p className='para'>
-          Select your customer then click on Create > Payment
+          On the <a href='https://manage.gocardless.com/customers' 
+          target='_blank' className='u-link-color-p u-text-underline'>customers section</a> of the dashboard, choose your customer
+          followed by Create &gt; Payment.
         </p>
 
         <img src='/images/faq/en/payment-create-buttons.png' />
 
         <p className='para'>
-          You can personalise your Direct Debit payment:
+          You'll be shown a screen from where you can personalise the Direct Debit payment:
         </p>
         <ul className='list'>
-          <li>Choose your amount (max €5,000)</li>
-          <li>The description lets you identify the reason why you collected this payment</li>
-          <li>The reference will appear on your customer bank statement (depending on their bank)</li>
-          <li>You can either take the Direct Debit payment as soon as possible or indicate a specific date</li>
+          <li>
+            Choose your payment amount (maximum of €5,000).
+          </li>
+          <li>
+            The description allows you to identify the reason why you've collected the payment.
+          </li>
+          <li>
+            The reference will normally appear on your customer's bank statement (the exact format will depend on your customer's bank).
+          </li>
+          <li>
+            You can either take the Direct Debit payment as soon as possible or you can indicate a specific date for the payment.
+          </li>
         </ul>
 
         <img src='/images/faq/en/payment-create.png' />
--- app/pages/faq/pro-dashboard/subscription-creation/faq-pro-dashboard-subscription-creation.en.js
+++ app/pages/faq/pro-dashboard/subscription-creation/faq-pro-dashboard-subscription-creation.en.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Translation from '../../../../components/translation/translation';
+import Link from '../../../../components/link/link';
 
 export default class FaqProDashboardSubscriptionCreationEn extends React.Component {
   displayName = 'FaqProDashboardSubscriptionCreationEn'
@@ -9,26 +10,62 @@ export default class FaqProDashboardSubscriptionCreationEn extends React.Compone
       <Translation locales='en'>
         <h2 className='u-text-heading-light u-text-m u-color-heading'>Subscription creation</h2>
         <p className='para'>
-          A subscription allows you to create a schedule of regular payments with a fixed amount: no need to
-          create the same payment month after month.
+          After setting up your customer, you'll be able to start taking Direct Debit payments from them. On
+          the <a href='https://manage.gocardless.com/customers' 
+          target='_blank' className='u-link-color-p u-text-underline'>customers section</a> of the dashboard, choose the customer
+          from whom you want to take payments.
+        </p>
+        <ul className='list'>
+          <li>
+            <strong>If you're taking payments of a varying amount or varying frequency</strong>, you should set up an individual payment.
+            For more information, see our <Link to='faq_pro_dashboard_payment_creation'
+            className='u-link-color-p u-text-underline'>tutorial on creating individual payments</Link>.
+          </li>
+          <li>
+            <strong>If you're taking payments of a regular amount at a regular frequency</strong>, you should set up a subscription
+            for the customer. This tutorial shows you how to create subscriptions.
+          </li>
+        </ul>
+
+        <h3 className='section-heading u-text-heading-light u-color-heading u-margin-Vm u-text-s'>
+          Creating a Subscription for your Customer
+        </h3>
+        <p className='para'>
+          A subscription allows you to collect regular payments of a fixed amount. There's no need to create the same payment 
+          week-after-week or month-after-month.
         </p>
         <p className='para'>
-          Select one of your customers, then click Create > Subscription.
+          On the <a href='https://manage.gocardless.com/customers' 
+          target='_blank' className='u-link-color-p u-text-underline'>customers section</a> of the dashboard, choose your customer
+          followed by Create &gt; Subscription.
         </p>
 
         <img src='/images/faq/en/subscription-create-buttons.png' />
 
         <p className='para'>
-          You can personalise your subscription:
+          You'll be shown a screen from where you can personalise the subscription:
         </p>
         <ul className='list'>
-          <li>Name your subscription</li>
-          <li>Choose the amount (max €5,000)</li>
-          <li>The reference will appear on the bank statement of your customer (depending on their bank)</li>
-          <li>Choose the frequency: weekly, monthly, yearly or personalised (e.g every 3 months)</li>
-          <li>Choose the date of the first Direct Debit payment</li>
-          <li>Choose the subscription duration: until either you cancel it, a specific date or a set number of
-          Direct Debit payments (for example 12 payments of €100)</li>
+          <li>
+            The subscription name allows you to identify the payment (e.g. it could be the name of your service or plan).
+          </li>
+          <li>
+            Choose your payment amount (maximum of €5,000). Once the subscription is set up, the same amount will be collected every time.
+          </li>
+          <li>
+            The reference will normally appear on your customer's bank statement (the exact format will depend on your customer's bank).
+          </li>
+          <li>
+            You can choose the frequency over which you collect your payments. For instance: weekly, monthly or yearly. You can also
+            define custom intervals (e.g. collecting a payment every 3 months).
+          </li>
+          <li>
+            You'll be able to choose the date of the first payment.
+          </li>
+          <li>
+            You can also choose the duration of the subscription. The subscription could continue until further notice, until a specific
+            date or only for a specific number of payments (e.g. after 12 payments are taken).
+          </li>
         </ul>
 
         <img src='/images/faq/en/subscription-create.png' />
