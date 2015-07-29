37eb320d10cd2510be9c051f53c2490c6b650095
Amaury De Closset <adeclosset@gmail.com>
2015-07-29 16:31:57 +0100

    removed unused var

--- app/pages/stories/stories/the-french-talents.js
+++ app/pages/stories/stories/the-french-talents.js
@@ -9,50 +9,37 @@ export default class StoriesTheFrenchTalents extends React.Component {
   render() {
     return (
       <StoryPage
-        title='Greater Anglia'
-        tagline='Greater Anglia saved £21,000 by switching to online Direct Debit with GoCardless'
+        title='Has Bean Coffee'
+        tagline='Has Bean coffee improved cashflow by 30% by moving to Direct Debit with GoCardless'
         tips={[
-            'We helped them save 26 hours of management time a month',
-            'We removed the need for paper mandates, saving £300 per month',
-            'We helped them avoid a £21,000 upgrade to their server infrastructure',
+            'We helped them reduce average payment period by 34%',
+            'GoCardless helped them identify bad debtors in advance',
+            'GoCardless helped them improve cash flow by 30%',
         ]}>
-        <StoryQuote tag='Collecting payments was complicated and very manual'>
-          <p className='para u-margin-Tn'>
-            <a href='http://www.greateranglia.co.uk' className='u-link-color-p u-text-underline' target='_blank'>
-            Greater Anglia</a>, the East Anglia rail franchise, uses Direct Debit to take monthly payments
-            allowing commuters to spread the cost of an annual season ticket.
-          </p>
-          <p className='para'>
-            Before using GoCardless, they managed their Direct Debit in-house for
-            eight years. “Collecting payments was complicated and very manual, and
-            keeping up with Direct Debit scheme rule changes was very time-consuming.”
-            It was also hard for the team to link their legacy Direct Debit management
-            processes with their online offering.
-          </p>
+        <StoryQuote tag='Customers paying invoices up to 4 months late drove us crazy'>
+          <p className='para u-margin-Tn'><a href='http://hasbean.co.uk' className='u-link-color-p u-text-underline' target='_blank'>
+          Has Bean Coffee</a> is a major supplier of high quality coffee to the retail and wholesale industries.
+          In an industry with lead times of up to a year, it is crucial for Has Bean to maintain strong cash flow.</p>
+          <p className='para'>With cafes often strapped for cash, "customers paying invoices up to four months late drove us crazy.
+          Our business was growing so fast and we were so busy that we would send out coffee to customers even with
+          long overdue invoices." Owner Steve Leighton was left with uncertain cash flow and numerous bad debtors.</p>
         </StoryQuote>
-        <StoryBigQuote heroClass='story-hero--greater-anglia' image='/images/testimonials/greater-anglia__avatar.png'
-        person='Graham Beton' position='Greater Anglia Railways'>
-          “GoCardless have taken all of the pain out of setting up and managing Direct Debit”
+        <StoryBigQuote heroClass='story-hero--has-bean-coffee' person='Steve Leighton'
+        position='Has Bean Coffee' img='/images/testimonials/hasbean-coffee__avatar.png'>
+          GoCardless gives us control over when we get paid and takes away the uncomfortable conversations about money.
         </StoryBigQuote>
-        <StoryQuote tag='Customers can now set up new season tickets entirely online'>
-          <p className='para u-margin-Tn'>Greater Anglia realised they needed an outsourced solution that could adapt to their
-          growing online sales. They quickly identified GoCardless as “by far the best option for
-          online Direct Debit”.</p>
-          <p className='para'>One key advantage was that because of our superior technology, GoCardless was the only
-          provider who could enable Greater Anglia’s customers to set up new season ticket customers
-          entirely online and link this back to their CRM and accounts.</p>
+        <StoryQuote tag='Now we can automatically take payment when issuing invoices'>
+          <p className='para u-margin-Tn'>Has Bean use GoCardless through accounting software, KashFlow. Now,
+          they can send out Direct Debit requests with invoices to all wholesale customers.
+          As soon as an invoice becomes due, they can automatically take payment from the customer.</p>
+          <p className='para'>As a bonus, the GoCardless integration with Kashflow means that these payments are
+          automatically reconciled in their accounts.</p>
         </StoryQuote>
-        <StoryQuote tag='Switching to GoCardless has saved us time and money'>
-          <p className='para u-margin-Tn'>
-            Switching to GoCardless' online system eliminated the need for paper mandates and statements,
-            saving Greater Anglia <strong>£300 every month</strong> and reducing management time
-            by <strong>26 hours</strong>. Greater Anglia also avoided a costly upgrade to their
-            server infrastructure otherwise needed to comply with the new Direct Debit rules.
-          </p>
-          <p className='para'>
-            Greater Anglia have been delighted with the service since making the switch:
-            “Their system is incredibly simple and their expertise is second to none.”
-          </p>
+        <StoryQuote tag='We now have control over when we get paid'>
+          <p className='para u-margin-Tn'>Since making the switch, Has Bean’s average payment period has been reduced from
+          62 days to 41 and cash flow has improved by 30%.</p>
+          <p className='para'>Thanks to GoCardless, Has Bean are now in control of when they get paid.
+          This has vastly improved their cashflow and helped them “keep uncomfortable conversations about money to a minimum”.</p>
         </StoryQuote>
       </StoryPage>
     );
