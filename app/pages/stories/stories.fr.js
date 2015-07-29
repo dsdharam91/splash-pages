2b360204aa5ed99073ab60ad7cd8b64cc858021f
Amaury De Closset <adeclosset@gmail.com>
2015-07-29 16:28:22 +0100

    Added 1001 Menus customer story

--- app/pages/stories/stories.fr.js
+++ app/pages/stories/stories.fr.js
@@ -15,17 +15,16 @@ export default class Stories extends React.Component {
             <div className='page-hero__inner stories-hero-heading'>
               <div className='page-hero__text'>
                 <h1 className='u-text-heading u-color-invert u-text-center u-text-xl u-text-light'>
-                  Thousands of happy customers
+                  Des milliers de clients satisfaits
                 </h1>
                 <div className='u-text-heading u-text-center u-color-invert u-text-m u-text-light u-margin-Txxs u-text-no-smoothing'>
-                  From individuals to multi-national corporations, GoCardless<br />
-                  helps thousands of businesses with their payments everyday
+                  Des startups aux multi-nationales, GoCardless<br />
+                  aident des milliers d'entreprises chaque jour.
                 </div>
               </div>
             </div>
           </div>
         </div>
-
 
         <div className='site-container u-padding-Vxxl u-margin-Bxxl'>
           <div className='u-padding-Vxxl u-relative u-margin-Bxxl'>
@@ -36,80 +35,36 @@ export default class Stories extends React.Component {
                   u-relative u-padding-Vl'>
                     <div className='stories-spotlight__item-text u-margin-Vl'>
                       <p className='u-padding-Al u-padding-Bn u-text-heading u-color-invert u-text-m'>
-                        “GoCardless gives us control over when<br />we get paid and takes away<br />
-                        the uncomfortable conversations<br />about money.”
+                        “GoCardless a complètement fluidifié le prélèvement comparé aux prestataires historiques.
+                        Nous avons réduit le temps passé sur nos paiements de xh et le taux d'impayés de x%.”
                       </p>
                       <div className='u-color-invert u-margin-Tm'>
-                        Steve Leighton, Has Bean Coffee
+                        Julien Balmont, 1001 Menus
                       </div>
                     </div>
                   </div>
-                  <div className='stories-spotlight__btn btn u-size-full'>Read the case study</div>
+                  <div className='stories-spotlight__btn btn u-size-full'>Voir la référence</div>
                 </Link>
                 <Link to='stories_the_french_talents' className='grid__cell u-text-center u-size-1of2
                 u-link-clean u-padding-Lxs u-padding-Rn'>
                   <div className='stories-spotlight__item stories-spotlight__item--crossfit-hackney u-relative u-padding-Vl'>
                     <div className='stories-spotlight__item-text u-margin-Vl'>
                       <p className='u-padding-Al u-padding-Bn u-text-heading u-color-invert u-text-m'>
-                        “Taking payments with GoCardless costs us less than half the price of using PayPal
-                        and the integration with TeamUp has definitely reduced our admin.”
+                        “En finir avec les retards de paiements a aidé à améliorer la trésorerie et
+                        a permis à nos équipes de se concenter sur des activités à plus forte valeur ajoutée.”
                       </p>
                       <div className='u-color-invert u-margin-Tm'>
-                        Geoff Stewart, Momentum Training
+                        Martin Gentil, Directeur commercial
                       </div>
                     </div>
                   </div>
-                  <div className='stories-spotlight__btn btn u-size-full'>Read the case study</div>
+                  <div className='stories-spotlight__btn btn u-size-full'>Voir la référence</div>
                 </Link>
               </div>
             </div>
           </div>
         </div>
 
-        <hr className='u-margin-An' />
-        <div className='site-container u-text-center u-padding-Vxxl'>
-          <div className='u-padding-Vxl'>
-            <Tabs triggers={[
-              (<a href='' className={'grid__cell u-padding-Hl stories-testimonials__link--guardian' +
-                 ' u-size-1of3 stories-testimonials__link u-link-clean'}>
-                <img src='/images/stories/guardian-logo@2x.jpg' />
-              </a>),
-              (<a href=''
-                 className={'grid__cell u-padding-Hl stories-testimonials__link--funding-circle' +
-                 ' u-size-1of3 stories-testimonials__link u-link-clean'}>
-                <img className='stories-testimonials__link-image--funding-circle' src='/images/stories/funding-circle-logo@2x.jpg' />
-              </a>),
-              (<a href=''
-                 className={'grid__cell u-padding-Hl stories-testimonials__link--lendable' +
-                 ' u-size-1of3 stories-testimonials__link u-link-clean'}>
-                <img className='stories-testimonials__link-image--lendable' src='/images/stories/lendable-logo@2x.jpg' />
-              </a>),
-            ]}>
-              <div className='stories-testimonials__quote stories-testimonials__quote--guardian u-relative u-margin-Txxl u-padding-Al'>
-                <p className="u-text-heading u-color-heading u-text-light u-text-m">
-                  "The Guardian is always looking to give the best customer experience and has chosen
-                  GoCardless to make its recurring payment experience
-                  as seamless as possible."
-                </p>
-              </div>
-              <div className={'stories-testimonials__quote stories-testimonials__quote--funding-circle' +
-                ' u-relative u-margin-Txxl u-padding-Al'}>
-                <p className="u-text-heading u-color-heading u-text-light u-text-m">
-                  "GoCardless is one of the best payments companies we've worked with.
-                  Their API was simple to integrate and allows us to use Direct Debit in a fully automated way.
-                  I'd highly recommend them to any company that cares about a providing good user experience and using the
-                  best available technology for their Direct Debit in the UK and Europe."
-                </p>
-              </div>
-              <div className='stories-testimonials__quote stories-testimonials__quote--lendable u-relative u-margin-Txxl u-padding-Al'>
-                <p className="u-text-heading u-color-heading u-text-light u-text-m">
-                  "We moved to GoCardless as collections can be managed by one person instead of a whole department.
-                  It's great to work with another financial technology company who are committed to helping businesses."
-                </p>
-              </div>
-            </Tabs>
-          </div>
-        </div>
         <hr className='u-margin-An' />
         <StartTakingPaymentsCTA />
       </Translation>
--- app/pages/stories/stories/1001-menus.js
+++ app/pages/stories/stories/1001-menus.js
@@ -9,49 +9,55 @@ export default class Stories1001Menus extends React.Component {
   render() {
     return (
       <StoryPage
-        title='Greater Anglia'
-        tagline='Greater Anglia saved £21,000 by switching to online Direct Debit with GoCardless'
+        title='1001 Menus'
+        tagline='1001 Menus a augmenté son chiffre d’affaire de  €X en automatisant le prélèvement via GoCardless'
         tips={[
-            'We helped them save 26 hours of management time a month',
-            'We removed the need for paper mandates, saving £300 per month',
-            'We helped them avoid a £21,000 upgrade to their server infrastructure',
+            'Nous leur avons permis d\'économiser xh de gestion administrative par mois',
+            'Nous les avons aidé à réduire les impayés de x%, augmentant le chiffre d\'affaire de x',
+            'Nous les avons aidé à réaliser de la micro-facturation pour de nouvelles offres',
         ]}>
-        <StoryQuote tag='Collecting payments was complicated and very manual'>
+        <StoryQuote tag='Le secteur des paiements est dominé par des logiciels complexes et archaïque'>
           <p className='para u-margin-Tn'>
-            <a href='http://www.greateranglia.co.uk' className='u-link-color-p u-text-underline' target='_blank'>
-            Greater Anglia</a>, the East Anglia rail franchise, uses Direct Debit to take monthly payments
-            allowing commuters to spread the cost of an annual season ticket.
+            <a href='http://www.1001menus.com' className='u-link-color-p u-text-underline' target='_blank'>
+            1001 Menus</a>, un outil tout-en-un pour gérer la communication de restaurants,
+            utilise le prélèvement pour encaisser les abonnements mensuels de ses clients, permettant
+            aux restaurateurs de ne pas avoir à se soucier d'envoyer à temps un chèque ou un virement.
           </p>
           <p className='para'>
-            Before using GoCardless, they managed their Direct Debit in-house for
-            eight years. “Collecting payments was complicated and very manual, and
-            keeping up with Direct Debit scheme rule changes was very time-consuming.”
-            It was also hard for the team to link their legacy Direct Debit management
-            processes with their online offering.
+            Avant GoCardless, ils géraient leur prélèvement via un autre prestataire spécialisé.
+            Le directeur technique explique : "Le processus précédent était lourd. Le client devait
+            valider le mandat par SMS ce qui lui faisait perdre du temps, et une fois le mandat validé,
+            il nous fallait déposer et récupérer des fichiers techniques sur un serveur FTP."
+            Il était difficile pour eux de faire le lien avec leur outil de facturation (Zuora) et de recevoir
+            les rejets avec motifs afin de réagir rapidement.
           </p>
         </StoryQuote>
-        <StoryBigQuote heroClass='story-hero--greater-anglia' image='/images/testimonials/greater-anglia__avatar.png'
-        person='Graham Beton' position='Greater Anglia Railways'>
-          “GoCardless have taken all of the pain out of setting up and managing Direct Debit”
+        <StoryBigQuote person='Julien Balmont' position='1001 Menus'>
+          GoCardless a complètement fluidifié le processus de prélèvement
         </StoryBigQuote>
-        <StoryQuote tag='Customers can now set up new season tickets entirely online'>
-          <p className='para u-margin-Tn'>Greater Anglia realised they needed an outsourced solution that could adapt to their
-          growing online sales. They quickly identified GoCardless as “by far the best option for
-          online Direct Debit”.</p>
-          <p className='para'>One key advantage was that because of our superior technology, GoCardless was the only
-          provider who could enable Greater Anglia’s customers to set up new season ticket customers
-          entirely online and link this back to their CRM and accounts.</p>
+        <StoryQuote tag="Nous voulions pouvoir faciliter l'expérience du client et notre gestion">
+          <p className='para u-margin-Tn'>1001 Menus s'est rendu compte que le processus actuel n'était
+          pas tenable avec leur croissance. Ils ont alors identifié GoCardless comme la meilleure
+          option pour le prélèvement.</p>
+
+          <p className='para'>
+          L'avantage clé fut notre technologie de pointe. GoCardless est le seul prestataire
+          de prélèvement qui pouvait automatiquement lier les prélèvements et les rejets directement
+          avec leur outil de facturation Zuora.</p>
         </StoryQuote>
-        <StoryQuote tag='Switching to GoCardless has saved us time and money'>
+        <StoryQuote tag="Passer à GoCardless nous a permis de gagner du temps et de l'argent">
           <p className='para u-margin-Tn'>
-            Switching to GoCardless' online system eliminated the need for paper mandates and statements,
-            saving Greater Anglia <strong>£300 every month</strong> and reducing management time
-            by <strong>26 hours</strong>. Greater Anglia also avoided a costly upgrade to their
-            server infrastructure otherwise needed to comply with the new Direct Debit rules.
+            1001Menus peut désormais gérer tous les mandats dans Zuora et déclencher les prélèvements
+            lorsque les factures viennent à échéance automatiquement. Mieux, le statut des paiements
+            avec motifs est automatiquement fourni dans Zuora, ce qui permet à 1001 Menus de relancer un client ou
+            de retenter un échec de paiement automatiquement. Cet automatisation a permis de diminuer
+            le taux d'impayé a diminué de <strong>x%</strong> et de faire gagner <strong>x h</strong> par mois
+            en temps administratif.
           </p>
           <p className='para'>
-            Greater Anglia have been delighted with the service since making the switch:
-            “Their system is incredibly simple and their expertise is second to none.”
+            Grâce à GoCardless, 1001 Menus a pu s'attaquer à de nouvelles opportunités de croissance:
+            "La facilité d'établir et d'émettre des prélèvements a ouvert de nouvelles
+            opportunités pour nous tel que la micro-facturation de nouveaux services annexes."
           </p>
         </StoryQuote>
       </StoryPage>
