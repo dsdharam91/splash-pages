7fd30ed4c3a34cb8fe812707194dc36c30fac291
Philip Harrison <philip@mailharrison.com>
2015-08-21 11:52:50 +0100

    Fix e2e

--- app/pages/home/home.e2e.js
+++ app/pages/home/home.e2e.js
@@ -3,25 +3,19 @@ require('babel/register');
 var BASE_URL = require('../../helpers/e2e/config').BASE_URL;
 var expectedConditions = require('../../helpers/e2e/expected-conditions');
 
-describe('Navigating to the Pro product', function() {
-  it('lets the user navigate to /pro', function() {
-    browser.get(BASE_URL);
-    element(by.id('track-nav-products')).click();
-    element(by.id('track-nav-pro')).click();
-    expect(browser.getCurrentUrl()).toEqual(BASE_URL + '/pro/');
-  });
-});
-
 describe('Swapping languages', function() {
   var ec = expectedConditions(browser);
 
   it('lets you navigate to a page and then swap languages', function() {
     browser.get(BASE_URL);
+
     element(by.id('track-nav-products')).click();
     element(by.id('track-nav-pro')).click();
 
     var popoverLink = element(by.cssContainingText('.popover-link', 'United Kingdom'));
     var frenchLink = element(by.css('a[href="/fr-fr/pro/"]'));
+
+    expect(browser.getCurrentUrl()).toEqual(BASE_URL + '/pro/');
 
     ec.waitForClickableThenClick(popoverLink).
       then(ec.waitForClickableThenClick.bind(null, frenchLink)).
