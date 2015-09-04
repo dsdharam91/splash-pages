6b1793e16315cb83657bc6c9fcfbb8ced74eba1a
Sam Willis <samueljohnwillis@gmail.com>
2015-09-04 17:57:50 +0100

    Implementing reassuring design.

--- app/pages/home/home.en.js
+++ app/pages/home/home.en.js
@@ -14,6 +14,9 @@ import TickSquareIcon from '../../icons/svg/tick-square';
 import ProIcon from '../../icons/svg/pro';
 import ShopIcon from '../../icons/svg/shop';
 import IntegrationsIcon from '../../icons/svg/integrations';
+import SecurityIcon from '../../icons/svg/security';
+import UsersIcon from '../../icons/svg/users';
+import MoneyHandIcon from '../../icons/svg/money-hand';
 
 export default class HomeEn extends React.Component {
   displayName = 'HomeEn'
@@ -25,12 +28,71 @@ export default class HomeEn extends React.Component {
           <div className='u-padding-Vxl'>
             <div className='u-size-2of3 u-center'>
               <h2 className='u-text-heading u-color-heading u-text-l u-text-light'>
+                Powering thousands, processing billions
+              </h2>
+            </div>
+            <div className='grid u-margin-Tl u-padding-Tm'>
+              <div className='grid__cell u-size-1of3 u-text-center'>
+                <figure className='svg-icon u-center'>
+                  <UsersIcon className='svg-icon__image svg-icon__image--shadow u-fill-green' />
+                <UsersIcon className='svg-icon__image u-fill-dark-gray' />
+                </figure>
+                <div className='u-text-heading u-color-heading u-text-light u-text-no-smoothing u-text-m u-margin-Tm'>
+                  12,000+ merchants
+                </div>
+                <p className='u-size-4of5 u-center u-color-p u-margin-Txs'>
+                  Over 12,000 merchants power their payments with GoCardless
+                </p>
+              </div>
+
+              <div className='grid__cell u-size-1of3 u-text-center'>
+                <figure className='svg-icon u-center'>
+                  <MoneyHandIcon className='svg-icon__image svg-icon__image--shadow u-fill-blue' />
+                  <MoneyHandIcon className='svg-icon__image u-fill-dark-gray' />
+                </figure>
+                <div className='u-text-heading u-color-heading u-text-light u-text-no-smoothing u-text-m u-margin-Tm'>
+                  Over $1bn transactions
+                </div>
+                <p className='u-size-4of5 u-center u-color-p u-margin-Txs'>
+                  We process more than $1 billion every year for our customers
+                </p>
+              </div>
+
+              <div className='grid__cell u-size-1of3 u-text-center'>
+                <figure className='svg-icon u-center'>
+                  <SecurityIcon className='svg-icon__image svg-icon__image--shadow u-fill-yellow' />
+                  <SecurityIcon className='svg-icon__image u-fill-dark-gray' />
+                </figure>
+                <div className='u-text-heading u-color-heading u-text-light u-text-no-smoothing u-text-m u-margin-Tm'>
+                  Military grade-secuirty
+                </div>
+                <p className='u-size-4of5 u-center u-color-p u-margin-Txs'>
+                  GoCardless encrypts all sensitive information & is FCA regulated
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr className='u-margin-An' />
+        <div className='u-text-center u-padding-Vxxl u-background-light-gray'>
+          <div className='site-container u-padding-Txl'>
+            <div className='u-size-1of2 u-center'>
+              <h2 className='u-text-heading u-color-heading u-text-light u-text-l'>
+                Trusted by thousands of businesses
+              </h2>
+            </div>
+            <div className='u-text-center u-margin-Vl u-padding-Vs u-center'>
+              <img src='/images/logos/pro-logos@2x.png' />
+            </div>
+          </div>
+        </div>
+        <hr className='u-margin-An' />
+        <div className='site-container u-text-center u-padding-Vxxl' id='learn-more'>
+          <div className='u-padding-Vxl'>
+            <div className='u-size-2of3 u-center'>
+              <h2 className='u-text-heading u-color-heading u-text-l u-text-light'>
                 Bringing Direct Debit into the 21st century
               </h2>
-              <p className='u-text-s u-color-p u-margin-Txs'>
-                GoCardless allows you to benefit from the great fees and low failure rates of Direct Debit,
-                without the operational complexity associated with traditional providers.
-              </p>
             </div>
             <div className='grid u-margin-Tl u-padding-Tm'>
               <div className='grid__cell u-size-1of3 u-text-center'>
@@ -148,23 +210,6 @@ export default class HomeEn extends React.Component {
             </div>
           </div>
         </Translation>
-        <hr className='u-margin-An' />
-        <div className='site-container u-text-center u-padding-Vxxl'>
-          <div className='u-padding-Txl'>
-            <div className='u-size-1of2 u-center'>
-              <h2 className='u-text-heading u-color-heading u-text-light u-text-l'>
-                Trusted by thousands of businesses
-              </h2>
-              <p className='u-text-s u-color-p u-margin-Txs'>
-                <Message pointer='home.trusted_message_scale' />
-                &nbsp;Whatever your scale, GoCardless works for you.
-              </p>
-            </div>
-            <div className='u-text-center u-margin-Vl u-padding-Vs u-center'>
-              <img src='/images/logos/pro-logos@2x.png' />
-            </div>
-          </div>
-        </div>
         <hr className='u-margin-An' />
         <div className='u-text-center u-padding-Vxxl'>
           <div className='site-container u-padding-Vxl'>
