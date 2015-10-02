84a431ab04d737955042eef9d868db780d49aff6
khlo <kin-hing@gocardless.com>
2015-10-02 09:37:40 +0100

    Change flags in footer to use PNG format

--- app/components/flag/flag.js
+++ app/components/flag/flag.js
@@ -1,39 +1,12 @@
 import React from 'react';
 
-import DEFlagIcon from '../../icons/svg/DE-flag';
-import IEFlagIcon from '../../icons/svg/IE-flag';
-import GBFlagIcon from '../../icons/svg/GB-flag';
-import FRFlagIcon from '../../icons/svg/FR-flag';
-import BEFlagIcon from '../../icons/svg/BE-flag';
-import EUFlagIcon from '../../icons/svg/EU-flag';
-import ESFlagIcon from '../../icons/svg/ES-flag';
-
-const countryMap = {
-  de: DEFlagIcon,
-  ie: IEFlagIcon,
-  gb: GBFlagIcon,
-  fr: FRFlagIcon,
-  be: BEFlagIcon,
-  eu: EUFlagIcon,
-  es: ESFlagIcon,
-};
-
 export default class Flag extends React.Component {
   displayName = 'Flag'
 
-  static propTypes = {
-    country: function(props, propName) {
-      const countries = Object.keys(countryMap);
-      if (!countries.indexOf(props[propName]) === -1) {
-        throw new TypeError(`Country must be in (${countries})`);
-      }
-    },
-  };
-
   render() {
-    const countryCode = this.props.country.toString().toLowerCase();
-    const FlagIcon = countryMap[countryCode];
-
-    return (<FlagIcon {...this.props} />);
+    const countryCode = this.props.country.toString();
+    const flagPath = `/images/flags/${countryCode}-flag-icon@2x.png`;
+    
+    return (<img src={flagPath} alt='' {...this.props} />);
   }
 }
