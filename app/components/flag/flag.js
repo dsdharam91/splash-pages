be91acd93e297072cdb71542178d8c6d040f3618
khlo <kin-hing@gocardless.com>
2015-10-02 09:51:47 +0100

    Fix the tests

--- app/components/flag/flag.js
+++ app/components/flag/flag.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { PropTypes } from '../../helpers/prop-types/prop-types';
+
 
 export default class Flag extends React.Component {
   displayName = 'Flag'
 
+  static propTypes = {
+    country: PropTypes.string.isRequired,
+  }
+
   render() {
     const countryCode = this.props.country.toString();
     const flagPath = `/images/flags/${countryCode}-flag-icon@2x.png`;
-    
+
     return (<img src={flagPath} alt='' {...this.props} />);
   }
 }
