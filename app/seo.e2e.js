f7a241bce031bd8aa5788fbb89f694ad4907ded6
Ken-Lo <ken@kenstechtips.com>
2015-10-26 19:20:19 +0000

    Some updates

--- app/seo.e2e.js
+++ app/seo.e2e.js
@@ -1,11 +1,15 @@
-const Promise = require('promise');
+/*eslint-disable */
 
 const countBy = require('lodash/collection/countBy');
 const filter = require('lodash/collection/filter');
 const forEach = require('lodash/collection/forEach');
 const pluck = require('lodash/collection/pluck');
+const pick = require('lodash/object/pick');
 const cheerio = require('cheerio');
 const request = require('superagent');
+
+var availableLocales = require('../config/available-locales');
+var getAllPaths = require('./router/route-helpers').getAllPaths;
 
 // Import this from app/static-build/builder
 function fetchPage(host, pagePath) {
@@ -27,102 +31,64 @@ function fetchPage(host, pagePath) {
 }
 
 var seoMatchers = {
-  toHaveValidTitle: function() {
+  toNotHaveDuplicateValues: function() {
     return {
-      compare: function(actual) {
+      compare: function(allPages, paramName) {
         var result = { pass: true, message: '' };
-        var failedUrls = [];
-        forEach(actual, function(pageInfo, pagePath) {
-          if (pageInfo.titleTagsNumberOf != 1) {
-            // Should have one <title> tag
-            failedUrls.push(pagePath+' (contained '+pageInfo.titleTagsNumberOf+' <title> tags)');
-            result.pass = false;
-          } else if (pageInfo.titleTagText.length > 70) {
-            // Should be less than 70 characters in length
-            failedUrls.push(pagePath+' (<title> tag was '+pageInfo.titleTagText.length+' characters long)');
-            result.pass = false;
-          }
+        var frequencyList = countBy(allPages, paramName);
+        var duplicatedValues = pick(frequencyList, function(freq) { return freq > 1; });
+        forEach(duplicatedValues, function(freq, value) {
+          var pagesWithThisValue = filter(allPages, paramName, value);
+          var pagePathsWithThisValue = pluck(pagesWithThisValue, 'pagePath');
+          result.message += value + ' (' + freq + ' times)\n' + pagePathsWithThisValue.join('\n') + '\n\n';
+          result.pass = false;
         });
-        if (!result.pass) {
-          result.message = 'The following pages had an invalid title:\n'+failedUrls.join('\n');
-        }
         return result;
-      }
-    }
+      },
+    };
   },
-  toHaveNonDuplicateTitles: function() {
+  toHaveNumberOfTags: function() {
     return {
-      compare: function(actual) {
+      compare: function(allPages, paramName, minValue, maxValue) {
         var result = { pass: true, message: '' };
-        var duplicatedTitles = [];
-        var titleTagFrequency = countBy(actual, 'titleTagText');
-        forEach(titleTagFrequency, function(freq, titleText) {
-          if (freq > 1) {
-            var pagePathsWithThisTitleText = pluck(filter(actual, 'titleTagText', titleText), 'pagePath');
-            duplicatedTitles.push(titleText+' ('+freq+' times)\n'+pagePathsWithThisTitleText.join('\n')+'\n');
-            result.pass = false;
-          }
+        var pagesWithError = pick(allPages, function(pageInfo) {
+          return pageInfo[paramName] < minValue || pageInfo[paramName] > maxValue;
         });
-        if (!result.pass) {
-          result.message = duplicatedTitles.join('\n');
-        }
+        forEach(pagesWithError, function(pageInfo) {
+          result.message += pageInfo.pagePath + ' (' + pageInfo[paramName] + ' tags)\n';
+          result.pass = false;
+        });
         return result;
-      }
-    }
+      },
+    };
   },
-  toHaveValidMetaDescription: function() {
+  toHaveTagValuesOfLength: function() {
     return {
-      compare: function(actual) {
+      compare: function(allPages, paramName, minLength, maxLength, isOptional) {
         var result = { pass: true, message: '' };
-        var failedUrls = [];
-        forEach(actual, function(pageInfo, pagePath) {
-          if (pageInfo.metaDescriptionTagsNumberOf > 1) {
-            // Should have no more than one <meta name="description"> tag
-            failedUrls.push(pagePath+' (contained '+pageInfo.metaDescriptionTagsNumberOf+' <meta name="description"> tags)');
-            result.pass = false;
-          } else if (pageInfo.metaDescriptionTagText.length < 100 || pageInfo.metaDescriptionTagText.length > 160) {
-            // Should be 100 to 160 characters in length
-            failedUrls.push(pagePath+' (<meta name="description"> tag was '+pageInfo.metaDescriptionTagText.length+' characters long)');
-            result.pass = false;
-          }
+        var pagesWithError = pick(allPages, function(pageInfo) {
+          return (!isOptional && pageInfo[paramName].length) &&
+            (pageInfo[paramName].length < minLength || pageInfo[paramName].length > maxLength);
         });
-        if (!result.pass) {
-          result.message = 'The following pages had an invalid description:\n'+failedUrls.join('\n');
-        }
-        return result;
-      }
-    }
-  },
-  toHaveNonDuplicateMetaDescriptions: function() {
-    return {
-      compare: function(actual) {
-        var result = { pass: true, message: '' };
-        var duplicatedMetaDescriptions = [];
-        var metaDescriptionFrequency = countBy(actual, 'metaDescriptionTagText');
-        forEach(metaDescriptionFrequency, function(freq, metaDescriptionText) {
-          if (freq > 1) {
-            var pagePathsWithThisMetaDescription = pluck(filter(actual, 'metaDescriptionTagText', metaDescriptionText), 'pagePath');
-            duplicatedMetaDescriptions.push(metaDescriptionText+' ('+freq+' times)\n'+pagePathsWithThisMetaDescription.join('\n')+'\n');
-            result.pass = false;
-          }
+        forEach(pagesWithError, function(pageInfo) {
+          result.message += pageInfo.pagePath + ' (' + pageInfo[paramName].length + ' characters)\n';
+          result.pass = false;
         });
-        if (!result.pass) {
-          result.message = duplicatedMetaDescriptions.join('\n');
-        }
         return result;
-      }
-    }
+      },
+    };
   },
-}
+};
 
 const serverUrl = '';
-const getAllPaths = [
+/*const getAllPaths = [
   'https://dl.dropboxusercontent.com/u/6414301/Test/normal_file.html',
-  'https://dl.dropboxusercontent.com/u/6414301/test/duplicate_file.html',
-  'https://dl.dropboxusercontent.com/u/6414301/test/long_title.html',
-  'https://dl.dropboxusercontent.com/u/6414301/test/long_title2.html',
+  //'https://dl.dropboxusercontent.com/u/6414301/test/duplicate_file.html',
+  'https://dl.dropboxusercontent.com/u/6414301/test/empty_desc.html',
+  /*'https://dl.dropboxusercontent.com/u/6414301/test/long_title2.html',
   'https://dl.dropboxusercontent.com/u/6414301/test/long_desc.html',
-  'https://dl.dropboxusercontent.com/u/6414301/test/long_desc_2.html'];
+  'https://dl.dropboxusercontent.com/u/6414301/test/long_desc_2.html'
+  ];*/
 const allPages = {};
 
 const processPage = function(pagePath) {
@@ -136,12 +102,12 @@ const processPage = function(pagePath) {
       const metaDescriptionTags = $('head').find('meta[name=description]');
       const h1Tags = $('body').find('h1');
       allPages[pagePath] = {
-        'pagePath': pageUrl,
-        'titleTagsNumberOf': titleTags.length,
-        'titleTagText': titleTags.first().text(),
-        'metaDescriptionTagsNumberOf': metaDescriptionTags.length,
-        'metaDescriptionTagText': metaDescriptionTags.first().attr('content'),
-        'h1TagsNumberOf': h1Tags.length,
+        pagePath: pageUrl,
+        titleTagsNumberOf: titleTags.length,
+        titleTagText: titleTags.first().text() || '',
+        metaDescriptionTagsNumberOf: metaDescriptionTags.length,
+        metaDescriptionTagText: metaDescriptionTags.first().attr('content') || '',
+        h1TagsNumberOf: h1Tags.length,
       };
     })
     .catch(console.error.bind(console)
@@ -150,20 +116,31 @@ const processPage = function(pagePath) {
 
 describe('SEO', function() {
   beforeAll(function(done) {
-    Promise.all(getAllPaths.map(processPage)).then(done).catch(console.error.bind(console));
+    Promise.all(getAllPaths(availableLocales).map(processPage)).then(done).catch(console.error.bind(console));
     jasmine.addMatchers(seoMatchers);
   });
 
-  it('all pages should have a title tag of less than 70 characters', function() {
-    expect(allPages).toHaveValidTitle();
+  describe('<title> tag', function() {
+    it('should have one on each page', function() {
+      expect(allPages).toHaveNumberOfTags('titleTagsNumberOf', 1, 1);
+    });
+    it('should be no more than 70 characters long', function() {
+      expect(allPages).toHaveTagValuesOfLength('titleTagText', 0, 70);
+    });
+    it('should not be duplicated on multiple pages', function() {
+      expect(allPages).toNotHaveDuplicateValues('titleTagText');
+    });
   });
-  it('all pages should have a unique title', function() {
-    expect(allPages).toHaveNonDuplicateTitles();
-  });
-  it('meta description should be between 100 and 160 characters in length', function() {
-    expect(allPages).toHaveValidMetaDescription();
-  });
-  it('all pages should have a unique meta description', function() {
-    expect(allPages).toHaveNonDuplicateMetaDescriptions();
+
+  describe('<meta name="description"> tag', function() {
+    it('should have no more than one on each page', function() {
+      expect(allPages).toHaveNumberOfTags('metaDescriptionTagsNumberOf', 0, 1);
+    });
+    it('should be 100 to 160 characters in length', function() {
+      expect(allPages).toHaveTagValuesOfLength('metaDescriptionTagText', 100, 160, true);
+    });
+    it('should not be duplicated on multiple pages', function() {
+      expect(allPages).toNotHaveDuplicateValues('metaDescriptionTagText');
+    });
   });
 });
