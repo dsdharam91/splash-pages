fd291a81aef3dbd4c3c4fcfe7de4487a85f92926
duncanbarrigan <duncanb@gocardless.com>
2015-11-02 21:38:53 +0000

    Edit to sales page

--- app/messages/en-SE.js
+++ app/messages/en-SE.js
@@ -44,10 +44,10 @@ export default {
     nav_title: 'Contact Sales',
     description: 'To find out more about how GoCardless can help you, please enter your details below. A member of our team will call you back as soon as possible.',
     page_title: 'Contact Sales',
-    page_description: 'To find out more about how GoCardless can help you, please enter your details below. A member of our team will call you back as soon as possible.',
+    page_description: 'To find out more about how GoCardless can help you, please enter your details below. A member of our team will call you back as soon as possible. We are currently beta testing our Autogiro service. Participants can collect Autogiro payments for free and access our standard Eurozone and UK services.',
     talk_to_us: 'Want to talk to an expert now?',
     call_us: 'Call us on',
-    information1: 'We are currently operating a trial of our Autogiro service.',
-    information2: 'Trial customers will receive free payments within Sweden as well as access to our full service across the Eurozone and the UK.',
+    information1: 'Your information is safe with us.',
+    information2: 'We won\'t ever share it or sell it to anyone.',    
   },
 };
