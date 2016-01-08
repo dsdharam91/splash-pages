2f1335a5234ca2ea427f9ca0aea8f4aab89fa238
Gabriela Matias <gabriela@gocardless.com>
2016-01-08 16:12:12 +0000

    Correct 1001 Menus to Zenchef

--- app/pages/about/about.fr.js
+++ app/pages/about/about.fr.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Message from '../../components/message/message';
 import Translation from '../../components/translation/translation';
 
 export default class AboutFr extends React.Component {
@@ -12,32 +11,51 @@ export default class AboutFr extends React.Component {
           <h2 className='u-text-heading u-color-heading u-text-l u-text-light u-padding-Txl'>
             A propos de GoCardless
           </h2>
-          <div className='grid'>
+
+         <div className='grid'>
             <div className='grid__cell u-size-2of3'>
-              <p className='u-text-s u-color-p u-margin-Txxl u-size-5of6'>
-                GoCardless est un des leaders européens du prélèvement bancaire.
-                Nous collectons plus d'un milliard d'Euros chaque année pour plus
-                de <Message pointer='number_of_merchants' /> entreprises et organisations dont le
-                Guardian, Omni Capital, Funding Circle et Pieminister, avec une croissance annuelle de 600%.<br /><br />
-                Nous nous sommes lancés en 2011 avec un seul objectif : rendre les prélèvements bancaires simples et accessibles à tous,
-                instantanément et à bas coût.
-                Les prélèvements bancaires étaient jusque-là réservés aux plus grandes entreprises et
-                requéraient beaucoup d'efforts à gérer.
-                Nous voulions changer cette situation et simplifier le prélèvement.
-                <br /><br />
-                Toute entreprise en France et en Europe peut désormais accéder au prélèvement bancaire grâce à
-                notre tableau de bord et notre API moderne et facile d'utilisation, et bénéficier d'un
-                mode de paiement économique avec un taux d'échec extrêmement bas.
-                <br /><br />
-                <span className='u-text-heavy u-color-heading'>Digne de confiance</span><br /><br />
-                GoCardless est un Etablissement de Paiement Agréé, autorisé par l’autorité britannique FCA (Financial Conduct Authority)
-                et habilité à prélever des paiements à travers l’Union Européenne.
-                Nous sommes financés par certains des plus importants investisseurs au monde
-                dont Balderton Capital, Accel Partners, Passion Capital et Y-Combinator.
-                Plus de <Message pointer='number_of_merchants' /> entreprises et organisations de toutes tailles utilisent GoCardless
-                pour collecter des millions d'Euros tous les jours.
+              <p className='para'>
+               Nous construisons un nouveau réseau de paiements pour Internet. Rejoignez-nous.
+              </p>
+              <p className='para'>
+            Depuis que GoCardless a été créé en 2011, nous avons cherché à simplifier
+        le prélèvement bancaire et à le rendre accessible aux entreprises qui
+        n'auraient jamais pu l’utiliser auparavant. Nos volumes ont déjà atteint
+<a href='http://www.forbes.com/sites/trevorclawson/2015/07/24/the-upward-march-of-fin-tech-gocardless-breaks-the-1bn-barrier/' target='_blank'>
+        plus d'1 milliard de $ annuels</a> pour des milliers d'entreprises,
+        et nous continuons d'avoir une très forte croissance.
+              </p>
+              <p className='para'>
+Nous aidons un large éventail d'entreprises: des salles de gym
+        et des espaces de co-working nous utilisent pour réduire leur
+        temps d’administration; des startups en forte croissance comme
+        Zenchef nous utilisent pour poursuivre leur croissance; des
+        institutions établies comme TripAdvisor nous utilisent pour réduire
+        considérablement leur taux d'échecs.
+              </p>
+              <p className='para'>
+                <a href='https://medium.com/@hirokitakeuchi/a-new-payment-network-for-the-internet-11ffb4d7165b' target='_blank'> Nos ambitions vont au-delà</a>:
+        nous voulons abattre les barrières afin que les
+        entreprises puissent rapidement et simplement
+        prélever un compte bancaire, n’importe où dans le monde.
+              </p>
+              <p className='para'>
+            Ces 12 derniers mois, nous avons élargi notre offre pour les
+        grandes entreprises, comme AXA Assistance et Habitat. Nous ajoutons
+        d’autres systèmes de prélèvements en Europe afin de permettre aux
+        multinationales d’encaisser des paiements partout en Europe.
+            Nous nous sommes récemment lancés en France et en Allemagne et
+        allons continuer notre expansion mondiale dans l'année à venir, avec
+des lancements prévus en Espagne, aux Pays-Bas et au-delà, pour créer un réseau
+mondial pour les paiements récurrents.
+              </p>
+              <p className='para'>
+                Nous sommes basés à Londres et sommes soutenus par certains
+des investisseurs européens les plus importants, comme Balderton Capital,
+Accel Partners, Passion Capital et Y Combinator.
               </p>
             </div>
+
             <div className='grid__cell u-size-1of3'>
               <div className='u-margin-Txxl'>
                 <img src='/images/press/publications-stacked.jpg' />
