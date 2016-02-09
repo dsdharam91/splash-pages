14cf57df54ab6cc8f09055dee244a39e0f174c06
Gabriela Matias <gabriela@gocardless.com>
2016-02-09 17:04:27 +0000

    Updates text on About us NL

--- app/pages/about/about.nl.js
+++ app/pages/about/about.nl.js
@@ -14,30 +14,55 @@ export default class AboutNl extends React.Component {
           <div className='grid'>
             <div className='grid__cell u-size-2of3'>
               <p className='para'>
-                GoCardless maakt het eenvoudig om doorlopende betalingen te incasseren.
-                We integreren alle incassonetwerken ter wereld en maken dit netwerk toegankelijk voor iedereen.
+                We bouwen een betalingsnetwerk voor het Internet. Vervoeg ons!
               </p>
               <p className='para'>
               <Translation locales='nl-NL'>
-                Sinds onze oprichting in 2011 zijn we snel uitgegroeid tot één van de marktleiders op het gebied van
-                automatische incasso in Europa. We verwerken jaarlijks meer dan een miljard euro voor meer dan 13.000 bedrijven en organisaties.
+                Sinds de oprichting van GoCardless in 2011, hebben we ons volledig gefocust op het
+                vereenvoudigen van incasso en op toegang geven aan bedrijven die dit vroeger niet konden gebruiken.
+                We verwerken reeds meer dan <a href='http://www.forbes.com/sites/trevorclawson/2015/07/24/the-upward-march-of-fin-tech-gocardless-breaks-the-1bn-barrier/'>
+                $1 miljard per jaar.</a> voor duizenden bedrijven, en we groeien snel.
               </Translation>
               <Translation locales='nl-BE'>
-                Sinds onze oprichting in 2011 zijn we snel uitgegroeid tot één van de marktleiders op het gebied van
-                domiciliëringen in Europa. We verwerken jaarlijks meer dan een miljard euro voor meer dan 13.000 bedrijven en organisaties.
+                Sinds de oprichting van GoCardless in 2011, hebben we ons volledig gefocust op het
+                vereenvoudigen van domiciliëringen en op toegang geven aan bedrijven die dit vroeger niet konden gebruiken.
+                We verwerken reeds meer dan <a href="http://www.forbes.com/sites/trevorclawson/2015/07/24/the-upward-march-of-fin-tech-gocardless-breaks-the-1bn-barrier/">
+                 $1 miljard per jaar </a>voor duizenden bedrijven, en we groeien snel.
               </Translation>
               </p>
               <p className='para'>
-                Kleine sportscholen en scouting clubs gebruiken ons om hun administratie te minimaliseren.
-                Snelgroeiende bedrijven als Funding Circle werken met ons om sneller te groeien.
-                En gevestigde merken als de Financial Times rekenen op ons om het aantal mislukte betalingen drastisch te verlagen.
+                We werken met een brede waaier van organisaties: kleine fitness zaken en scout groepen werken met ons om
+                hun administratie te minimaliseren; snel groeiende bedrijven zoals Funding Circle gebruiken ons om hun groei
+                te ondersteunen; gevestigde merken zoals de Financial Times rekenen op ons in het percentage mislukte
+                betalingen dramatisch te verlagen.
               </p>
               <p className='para'>
-                We zijn een klein team in Londen en worden gefinancierd door de beste investeerders ter wereld
-                als Balderton Capital, Accel Partners en Y&nbsp;Combinator.
+                Onze ambities reiken <a href="https://medium.com/@hirokitakeuchi/a-new-payment-network-for-the-internet-11ffb4d7165b"> verder</a>:
+                we verwijderen alle belemmeringen zodat bedrijven eenvoudig en snel betalingen van eender wie, eender waar in de wereld kunnen ontvangen.
               </p>
-            </div>
-            <div className='grid__cell u-size-1of3'>
+              <p className='para'>
+              <Translation locales='nl-NL'>
+                In het afgelopen jaar hebben we onze oplossingen voor grotere bedrijven, zoals The Guardian en de Engelse
+                overheid, uitgebreid. We hebben tevens Europese incasso schema’s toegevoegd, zodat multinationale
+                bedrijven eenvoudig betalingen kunnen incasseren in heel Europa. We zijn onlangs in Frankrijk gestart
+                en in het komende jaar zetten we onze wereldwijde uitbreiding verder met lanceringen Duitsland, Nederland,
+                Zweden en vele andere markten, om zo een wereldwijd netwerk voor terugkerende betalingen te bouwen.
+              </Translation>
+              <Translation locales='nl-BE'>
+                In het afgelopen jaar hebben we onze oplossingen voor grotere bedrijven, zoals The Guardian en de Engelse
+                overheid, uitgebreid. We hebben tevens schema’s voor Europese domiciliëring toegevoegd, zodat
+                multinationale bedrijven eenvoudig betalingen kunnen incasseren in heel Europa. We zijn onlangs
+                in Frankrijk gestart en in het komende jaar zetten we onze wereldwijde uitbreiding verder
+                met lanceringen Duitsland, Nederland, België, Zweden en vele andere markten, om zo een wereldwijd
+                netwerk voor terugkerende betalingen te bouwen.
+              </Translation>
+              </p>
+              <p className='para'>
+                We zijn in Londen gevestigd, en we zijn gefinancierd door enkele van de beste investeerders in de wereld,
+                onder andere Balderton Capital, Accel Partners, Passion Capital en Y Combinator.
+              </p>
+              </div>
+              <div className='grid__cell u-size-1of3'>
               <div className='u-margin-Txxl'>
                 <img src='/images/press/publications-stacked.jpg' />
                 <p className='u-text-xs u-color-dark-gray u-margin-Txxl'>
@@ -46,8 +71,8 @@ export default class AboutNl extends React.Component {
                 </p>
               </div>
             </div>
+           </div>
           </div>
-        </div>
      </Translation>
     );
   }
