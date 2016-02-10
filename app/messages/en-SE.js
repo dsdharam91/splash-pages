7743b686ea279dd5b6beb7a6437e107a47e63250
duncanbarrigan <duncanb@gocardless.com>
2016-02-10 15:50:47 +0000

    Pricing page edits

--- app/messages/en-SE.js
+++ app/messages/en-SE.js
@@ -16,16 +16,16 @@ export default {
   },
   hero: {
     header: 'Accept recurring payments across Europe',
-    desc: 'GoCardless allows you to collect Direct Debit (Autogiro) payments from Sweden, the Eurozone and the UK in a single integration.',
+    desc: 'GoCardless allows you to collect Autogiro (Direct Debit) payments from Sweden, the Eurozone and the UK in a single integration.',
   },
   pricing: {
     cost_cap: '20 kr',
     description: 'Collect Direct Debit payments online with fees of just 1%, capped at 20 kr. Scale pricing is available for larger organisations.',
-    pro_cost_per_transaction: '0,50 - 6 kr',
+    pro_cost_per_transaction: '0,50 - 5 kr',
     per_transaction_amount_normal: '1%',
-    pro_monthly_fee: '2 500 kr',
-    normal_capped_at_amount: '2000 kr',
-    pro_number_of_monthly_payments: '>500 payments',
+    pro_monthly_fee: '2500kr',
+    normal_capped_at_amount: '2000kr',
+    pro_number_of_monthly_payments: '> 500',
   },
   basic_pricing_cta: {
     main: 'Pay 1%',
