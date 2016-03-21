f73e60e5daa62c5785a573b54093aaca6c640b04
Ken-Lo <ken@kenstechtips.com>
2016-03-21 14:36:09 +0000

    Update messages for fr and es

--- app/messages/es.js
+++ app/messages/es.js
@@ -1,5 +1,5 @@
 export default {
-  number_of_merchants: '14.000',
+  number_of_merchants: '16.000',
   payments_processed_volume: '€1.000 millones',
   header: {
     our_products: 'Nuestros Productos',
