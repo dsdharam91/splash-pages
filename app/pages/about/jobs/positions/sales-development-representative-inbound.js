99e446a59b08d6203de49b20e86aa9330c73368e
Gabriela Matias <gabriela@gocardless.com>
2016-04-23 14:37:10 +0100

    Fix paragraph formatting for SDR Inbound

--- app/pages/about/jobs/positions/sales-development-representative-inbound.js
+++ app/pages/about/jobs/positions/sales-development-representative-inbound.js
@@ -35,7 +35,7 @@ export default class SalesDevelopmentRepresentativeInbound extends React.Compone
           business and product leaders across the company to help accelerate
           our growth.
         </p>
-        <p>
+        <p className='para'>
           Training for the position will involve a 4 week structured program split across both our NBD team as
           well as our Sales Development team. The program will give you an in depth overview of the product,
           how we do sales at GoCardless, and a detailed understanding of how we fit into the wider payments industry.
