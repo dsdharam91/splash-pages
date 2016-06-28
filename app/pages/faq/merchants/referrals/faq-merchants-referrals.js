9263793a7dc29095136aa19e0b4a1ff8ed5863ac
Ken-Lo <ken@kenstechtips.com>
2016-06-28 17:30:44 +0100

    Update referral FAQ

--- app/pages/faq/merchants/referrals/faq-merchants-referrals.js
+++ app/pages/faq/merchants/referrals/faq-merchants-referrals.js
@@ -25,32 +25,39 @@ export default class FaqMerchantsReferrals extends React.Component {
           What happened to the old referral scheme?
         </h3>
         <p className='para'>
-          Our old referral scheme ceased to function from February 29th, 2016. All sign-ups made on or after this date no longer
-          qualify for the referral incentive.
+          Our previous referral scheme was closed on February 29th, 2016. All sign-ups made on or after this date no longer qualify
+          for the referral incentive.
         </p>
 
         <h3 className='u-text-heading-light u-color-dark-gray u-margin-Vm u-text-s'>
           I’ve referred a friend to GoCardless. What happens to my GoCardless credit?
         </h3>
         <p className='para'>
-          If you referred a friend and they signed up before February 29th 2016, you will receive your payout for February, March and April 2016.
-          We will be emailing you directly with information on receiving any outstanding rebates post April 2016.
+          Our old referral scheme is now closed and payouts from that referral scheme will stop from the 30th June 2016.
+          Instead we will be offering all those businesses who have outstanding credits from the referral scheme a rebate of transaction
+          fees for all transactions processed by GoCardless from 1st July 2016 until the 30th November 2016, subject to fair use
+          (and other detail as set out in the T&Cs). All eligible businesses will receive an email to confirm that they will receive a
+          one-off credit of the cumulative transaction fee amount by the 16th December 2016 - please note this will be processed
+          automatically and paid out to the bank account we hold on file for you on the payment date.
         </p>
 
         <h3 className='u-text-heading-light u-color-dark-gray u-margin-Vm u-text-s'>
           I’ve joined GoCardless through a referral link. What happens to my fee rebate?
         </h3>
         <p className='para'>
-          If you signed up through a friend’s referral link before February 29th 2016, you will receive your payout for February and March 2016.
-          We will be emailing you directly with information on receiving any outstanding rebates post March 2016.
+          If you signed up through a friend’s referral link before February 29th 2016 and still have outstanding credits owed, you will
+          also be eligible for a rebate of transactions fees for all transactions processed by GoCardless from 1st July 2016 until
+          the 30th November 2016, subject to fair use (and other detail as set out in the T&Cs). All eligible businesses will receive an
+          email to confirm that they will receive a one off credit of the cumulative transaction fee amount by the 16th December 2016 -
+          please note this will be processed automatically and paid out to the bank account we hold on file for you on the payment date.
         </p>
 
         <h3 className='u-text-heading-light u-color-dark-gray u-margin-Vm u-text-s'>
           What happens to my old referral links?
         </h3>
         <p className='para'>
-          Your old GoCardless referral links ceased to function from February 29th, 2016. Anyone signing up through your referral link
-          from this date will see a message on the sign-up page stating the scheme is no longer available.
+          Our previous referral scheme was closed on February 29th, 2016. Anyone signing up through a referral link from this programme
+          after this date will see a message on the sign-up page stating the scheme is no longer available.
         </p>
 
         <p className='para'>&nbsp;</p>
