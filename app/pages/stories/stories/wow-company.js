8bca8e223eba91c17f87610c7b41f49e4d5804f6
Ken-Lo <ken@kenstechtips.com>
2016-10-31 16:53:33 +0000

    Update Wow Company copy

--- app/pages/stories/stories/wow-company.js
+++ app/pages/stories/stories/wow-company.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import StoryPageNew from '../story-page-new';
+import Link from '../../../components/link/link';
 
 export default class StoriesWowCompany extends React.Component {
   displayName = 'StoriesWowCompany'
@@ -7,121 +8,98 @@ export default class StoriesWowCompany extends React.Component {
   render() {
     return (
       <StoryPageNew
-        title='For ongoing monthly payments it’s perfect. You can easily adjust the subscription amounts and don’t suffer from the problem of expired cards'
-        author='Charles Cridland'
-        position='Technical Director'
-        company='YourParkingSpace'
-        benefits='Affordable fees, seamless integration, an easy-to-use customer experience and countless hours saved'
+        title='I couldn’t quite believe that a system had been created that solved almost a decade of billing and payment headaches for us. Ever since that day, we’ve never looked back.'
+        author='Peter Czapp'
+        position='Co-Founder and Director'
+        company='The Wow Company'
+        benefits='TBC'
         bgimgUrl='url(/images/stories/story-images/wow-company/hero-wow-company.jpg)'
         sidebarLogoSrc='/images/stories/story-logos/the-wow-company.png'
-        sidebarServices='An online marketplace designed to make parking your car easier'
-        sidebarEmployees='10 employees'
-        sidebarTimeSaved='Countless hours!'>
+        sidebarServices='TBC'
+        sidebarEmployees='TBC employees'
+        sidebarTimeSaved='TBC'>
         <div className='story-body'>
           <p className='para u-margin-Tn'>
-            It’s always inspirational to find a business that’s followed its own
-            path. <a href='http://www.thewowcompany.com/' className='u-link-color-p u-text-underline'>The Wow Company</a> is a firm of
-            cloud accountants and business advisers that’s done just that – by taking the boring out of bookkeeping and making small
-            business finance something to get excited about.
+            <a href='http://www.thewowcompany.com/' className='u-link-color-p u-text-underline'>The Wow Company</a> is a firm of
+            cloud accountants and business advisers that aims to take the boring out of bookkeeping and make small business finance
+            something to get excited about.
           </p>
-          <p className='para'>
-            We sat down with Peter Czapp, co-founder and director of The Wow Company, to hear more about their ambitious plans to put technology at the centre of every small business, and how GoCardless has transformed the way they run their firm.
-          </p>
-          <h2 className='u-text-heading u-color-dark-gray u-text-l u-text-light u-margin-Tn'>
-            Being a business adviser that helps small businesses
+
+          <h2 className='u-text-heading u-color-dark-gray u-text-l u-text-light u-margin-Txl'>
+            Advising small businesses
           </h2>
           <p className='para'>
-            Technology and cloud computing has changed the business landscape and changed the possibilities for small businesses. 
+            The role of the accountant in the business world is changing. Computers and technology are automating so many of the
+            things accountants did in the past. The role of the accountant is now about being an adviser.
           </p>
           <p className='para'>
-            Financial software tools are now intuitive, easy to use and let business owners see their financial health simply, efficiently and elegantly. And that’s changed people’s expectations of their accountant, as Peter explains. 
+            “At Wow, we help businesses look forward, not just back at last year’s numbers. And a big part of that ‘looking forward’
+            mentality is to give them the right tools and advice to help them do what they do best,” said Peter.
           </p>
           <p className='para'>
-            “The role of the accountant in the business world is changing. Computers and technology are automating so many of the things accountants did in the past. And what that means – and what we at Wow believe – is that the role of the accountant is now about being an adviser. 
+            This prompted Peter and his co-founder, Paul Bulpitt, to set up The Wow Company in 2004, with a team that’s at one with
+            technology, has a real focus on amazing customer service and strives to make a genuine difference for clients.
           </p>
-          <p className='para'>
-            At Wow, we’re here to help businesses look forward, not just back at last year’s numbers. And a big part of that ‘looking forward’ mentality is to give them the right tools and advice to help them do what they do best. They went into business to use their skills – not to faff around with their accounts.”
-          </p>
-          <p className='para'>
-            And this change is what prompted Peter and his co-founder, Paul Bulpitt, to set up The Wow Company in 2004, with a team that’s totally at one with technology, has a real focus on amazing customer service and strives to make a genuine difference for clients.
-          </p>
-          <h2 className='u-text-heading u-color-dark-gray u-text-l u-text-light u-margin-Tn'>
-            Finding the right clients with the right mindset
+
+          <h2 className='u-text-heading u-color-dark-gray u-text-l u-text-light u-margin-Txl'>
+            Seek out the best tech
           </h2>
           <p className='para'>
-            The key to the success of The Wow Company was finding clients who share Peter’s forward-thinking, future-ready outlook. 
+            The Wow Company’s ethos is all about stressing the importance of small businesses seeking out new technology that makes their
+            lives easier. This is how Peter first discovered GoCardless – while looking for a solution to solve cash-collection issues.
           </p>
           <p className='para'>
-            “We believe in explaining our business philosophy, sharing what we’re passionate about and trusting that like-minded people will come to us. In the early days we had to seek people out, but now, after many years of refining our offering, we’re very fortunate in that we have a growing client base and a great reputation – and that means that people come to us. 
+            “As a business, we have recurring billing. A decade ago, that meant going into a Word template, changing the dates,
+            changing the amounts and putting the invoice into an email – and that was a nightmare. You’d spend days doing it each month.
           </p>
           <p className='para'>
-            They know we’re going to help them move their business forward, give them advice on streamlining their finances and make their life easier. Our clients are the trailblazers; they’re doing incredible stuff and they’re breaking new ground, and because of that they’re incredible fun to work with – and we wouldn’t have it any other way.”
+            Then <a href='http://www.xero.com/' className='u-link-color-p u-text-underline'>Xero online accounting software</a> came along
+            and automated that whole billing process, but clients were still using standing orders to pay those fees.
+            Every time a client changed their monthly amount, or had additional billing, that
+            standing order had to be changed – creating a nightmare that had no benefit for us or our clients.”
           </p>
-          <h2 className='u-text-heading u-color-dark-gray u-text-l u-text-light u-margin-Tn'>
-            Seek out the best tech for your business
+
+          <h2 className='u-text-heading u-color-dark-gray u-text-l u-text-light u-margin-Txl'>
+            A flexible payment solution
           </h2>
           <p className='para'>
-            The Wow Company’s ethos is all about stressing the importance of small businesses seeking out new technology that makes their lives easier. And this is how Peter first came into contact with GoCardless – while looking for a solution to solve their cash-collection issue.
+            Peter started looking around at better ways to collect company fees. He knew Direct Debit was a great option, but had
+            been put off by the £25k bond the bank had insisted on – not to mention the unlimited personal guarantee they were asking for.
           </p>
           <p className='para'>
-            “As a business, we have recurring billing. A decade ago, that meant going into a Word template, changing the dates, changing the amounts and putting the invoice into an email – and that was a nightmare. You’d spend days doing it each month.
+            “We felt there had to be a better way to collect those monthly fees. So I did a Google search for Direct Debit providers
+            and up popped the GoCardless site,” Peter said.
           </p>
           <p className='para'>
-            Then Xero online accounting software came along and automated that whole billing process. What used to take us days now happened overnight, but clients were still using standing orders to pay those fees. So every time a client changed their monthly amount, or had additional billing, that standing order had to be changed – creating a nightmare that had no benefit for us or our clients.”
+            Within less than 20 minutes he was set up, had collected an amount on his personal credit card and had completed the
+            payment process.
           </p>
-          <h2 className='u-text-heading u-color-dark-gray u-text-l u-text-light u-margin-Tn'>
-            A payment solution with the right flexibility
-          </h2>
-          <p className='para'>
-            Faced with the challenge of dealing with these standing orders, Peter started looking around at better ways to collect their fees. He knew Direct Debit was a great option, but had been put off by the £25k bond their bank had insisted on – not to mention the unlimited personal guarantee they were asking for. Then a serendipitous search on Google brought him the answer.
+          <p className='story-page__quote'>
+            “I was just amazed and in awe. I couldn’t quite believe that a system had been created that solved almost a decade of
+            billing and payment headaches for us. Ever since that day, we’ve never looked back.”
           </p>
-          <p className='para'>
-            “In early 2013, we’d set up another business, The Agency Collective, that has a monthly membership subscription fee. We felt there had to be a better way to collect those monthly fees. So I did a Google search for Direct Debit providers and up popped the GoCardless site. 
-          </p>
-          <p className='para'>
-            I sat there and following the sign-up instructions and within less than 20 minutes I was set up, had collected an amount on my personal credit card and had completed the payment process.
-          </p>
-          <p className='para'>
-            I have to be honest, I was just amazed and in awe. I couldn’t quite believe that a system had been created that solved almost a decade of billing and payment headaches for us. Ever since that day, we’ve never looked back.”
-          </p>
-          <h2 className='u-text-heading u-color-dark-gray u-text-l u-text-light u-margin-Tn'>
+
+          <h2 className='u-text-heading u-color-dark-gray u-text-l u-text-light u-margin-Txl'>
             How GoCardless transformed the business
           </h2>
           <p className='para'>
-            Part of what Peter really loves about GoCardless is that it doesn’t just collect all the retainer fees, but the payments for ad-hoc work throughout the month too. 
+            Part of what Peter really loves about GoCardless is that it doesn’t just collect all the retainer fees, but the payments
+            for ad-hoc work throughout the month too.
           </p>
           <p className='para'>
-            “We’re connected up with Directli, so when we bill for additional work, that money is collected 48 hours later and we don’t have to lift a finger. It means that we don’t have to employ a finance team to chase our debts, and that’s a cost-saving we can invest back into the business and into improving the impact we have for clients.
+            “We’re connected up using <Link to='partners_xero' className='u-link-color-p u-text-underline'>GoCardless for Xero</Link>,
+            so when we bill for additional work, that money is collected 48 hours later. We don’t have to lift a finger.
+            It means that we don’t have to employ a finance team to chase our debts, and that’s a cost-saving
+            we can invest back into the business and into improving the impact we have for clients.
           </p>
           <p className='para'>
-            Everything’s taken care of as we sleep and our people can focus on higher-value tasks and client relationships, rather than chasing bills. So GoCardless, as a solution, has just been an absolute revelation for our business and I cannot imagine life without it now.
+            Our people can focus on higher-value tasks and client relationships, rather than chasing bills. GoCardless, as a solution,
+            has just been an absolute revelation for our business and I can’t imagine life without it now."
           </p>
-          <p className='para'>
-            Moving our payment collection over to GoCardless really had been transformational for us. We’re a completely different business as a result of GoCardless and the second we discovered it we started recommending it to our clients. For anyone who wants to sharpen up their cash-collection function it really is an astonishing tool.”
-          </p>
-          <h2 className='u-text-heading u-color-dark-gray u-text-l u-text-light u-margin-Tn'>
-            Putting the client experience first
-          </h2>
-          <p className='para'>
-            Giving small business owners advice that helps them set the right foundations and use the right technology is central to what The Wow Company does. But, for Peter, the customer-focused, caring attitude of their team is just as important.
-          </p>
-          <p className='para'>
-            “There’s something really special about the team we have – and that’s the essence of what makes Wow different to other accountants. Our mission is to deliver world-famous customer service. We genuinely care for our clients and go out of our way to make their lives easier. There are 35 amazing people in our team who deliver amazing work for clients – and that’s our USP. It’s that team. We will look after your business like no-one else in the world.
-          </p>
-          <p className='para'>
-            We have a Head of Customer Experience – that’s unheard of in an accountancy firm! Anyone can crunch the numbers, but if you want something magical, someone who’s a real business partner to guide your journey, then there’s nothing like the experience of being a Wow client.”
-          </p>
-          <h2 className='u-text-heading u-color-dark-gray u-text-l u-text-light u-margin-Tn'>
-            The future for Wow – the next five years
-          </h2>
-          <p className='para'>
-            So, with so much enthusiasm and energy behind them, where does Peter see The Wow Company being in five years’ time? What does the future hold for the firm?
-          </p>
-          <p className='para'>
-            “Although we’ve been going for nearly 13 years, I feel like it’s only just started. We want to be the champions for small businesses and make a massive difference for them. There’s so much technology that hasn’t been invented yet, that could help our clients on their business journeys. And I can promise you that for every day of the next five years that we’ll be seeking out that technology. 
-          </p>
-          <p className='para'>
-            We’ll be looking for ways to innovate, to make our service even better, to make our customer service even slicker and more valuable for clients. We won’t rest on this quest to be the best that we can be and to help our clients to thrive and grow.”
+          <p className='story-page__quote'>
+            "Moving payment collection to GoCardless has been transformational for us. We’re a completely different business as a
+            result of GoCardless and the second we discovered it we started recommending it to our clients. For anyone who wants to
+            sharpen up their cash-collection function it really is an astonishing tool.”
           </p>
         </div>
       </StoryPageNew>
