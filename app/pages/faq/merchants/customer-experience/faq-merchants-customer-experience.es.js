2a689eefa16ab8bdbb3aa66eb567f24e98a0e1bb
Ken-Lo <ken@kenstechtips.com>
2016-11-17 16:43:32 +0000

    Improve formatting on ES customer experience FAQ

--- app/pages/faq/merchants/customer-experience/faq-merchants-customer-experience.es.js
+++ app/pages/faq/merchants/customer-experience/faq-merchants-customer-experience.es.js
@@ -7,37 +7,47 @@ export default class FaqMerchantsCustomerExperienceEs extends React.Component {
   render() {
     return (
       <Translation locales='es'>
-        <h2 className='u-text-heading-light u-text-m u-color-dark-gray'>Experiencia de Usuario<br></br></h2>
-          <h3 className='u-text-heading-light u-color-dark-gray u-margin-Vm u-text-s'>
+        <h2 className='u-text-heading-light u-text-m u-color-dark-gray'>
+          <strong>Experiencia de Usuario</strong>
+        </h2>
+        <h3 className='u-text-heading-light u-color-dark-gray u-margin-Txl u-margin-Bm u-text-s'>
+          <strong>
             ¿Puedo rellenar el mandato SEPA online en nombre de mi cliente?
-          </h3>
-          <p className='para'>
-            No - debido a las estrictas restricciones en la Domiciliación Bancaria SEPA, los pagadores deben
-            rellenar sus datos de cobro personalmente.<br></br>
-          </p>
+          </strong>
+        </h3>
+        <p className='para'>
+          No - debido a las estrictas restricciones en la Domiciliación Bancaria SEPA, los pagadores deben
+          rellenar sus datos de cobro personalmente.
+        </p>
 
-          <h3 className='u-text-heading-light u-color-dark-gray u-margin-Vm u-text-s'>
+        <h3 className='u-text-heading-light u-color-dark-gray u-margin-Txl u-margin-Bm u-text-s'>
+          <strong>
             ¿Aparecerá el nombre de mi empresa en el extracto bancario del cliente?
-          </h3>
-          <p className='para'>
-            Con GoCardless, el nombre de tu empresa aparecerá en el campo de referencia de la transacción y GoCardless
-            aparecerá como el emisor. Con GoCardless Pro, solo aparecerá el nombre de tu negocio en la transacción.
-          </p>
-          <p className='para'>
-            Puedes personalizar como mostramos el nombre de tu negocio en los extractos bancarios de tus clientes
-            en los "Settings" de tu panel de control.<br></br>
-          </p>
+          </strong>
+        </h3>
+        <p className='para'>
+          Con GoCardless, el nombre de tu empresa aparecerá en el campo de referencia de la transacción y GoCardless
+          aparecerá como el emisor. Con GoCardless Pro, solo aparecerá el nombre de tu negocio en la transacción.
+        </p>
+        <p className='para'>
+          Puedes personalizar como mostramos el nombre de tu negocio en los extractos bancarios de tus clientes
+          en los "Settings" de tu panel de control.
+        </p>
 
-        <h3 className='u-text-heading-light u-color-dark-gray u-margin-Vm u-text-s'>
-          ¿Cómo me puede pagar el usuario?
+        <h3 className='u-text-heading-light u-color-dark-gray u-margin-Txl u-margin-Bm u-text-s'>
+          <strong>
+            ¿Cómo me puede pagar el usuario?
+          </strong>
         </h3>
         <p className='para'>
           Tu cliente puede introducir sus datos bancarios online a través de GoCardless; si utilizas GoCardless Pro
-          también puede hacerlo por teléfono o por escrito.<br></br>
+          también puede hacerlo por teléfono o por escrito.
         </p>
 
-        <h3 className='u-text-heading-light u-color-dark-gray u-margin-Vm u-text-s'>
-          ¿Puedo alojar las páginas de cobro en mi sitio web?
+        <h3 className='u-text-heading-light u-color-dark-gray u-margin-Txl u-margin-Bm u-text-s'>
+          <strong>
+            ¿Puedo alojar las páginas de cobro en mi sitio web?
+          </strong>
         </h3>
         <p className='para'>
           De nuevo, depende de los productos que elijas. Con GoCardless, para cumplir con las normas del esquema de
@@ -46,30 +56,36 @@ export default class FaqMerchantsCustomerExperienceEs extends React.Component {
           acceder a través de una ventana emergente desde tu sitio.
         </p>
         <p className='para'>
-          Con GoCardless Pro, puedes albergar las páginas de pago en tu propio sitio y no tendrás que redirigir a tus clientes.<br></br>
+          Con GoCardless Pro, puedes albergar las páginas de pago en tu propio sitio y no tendrás que redirigir a tus clientes.
         </p>
 
-        <h3 className='u-text-heading-light u-color-dark-gray u-margin-Vm u-text-s'>
-          ¿Ofrecéis iFrames?
+        <h3 className='u-text-heading-light u-color-dark-gray u-margin-Txl u-margin-Bm u-text-s'>
+          <strong>
+            ¿Ofrecéis iFrames?
+          </strong>
         </h3>
         <p className='para'>
           No - no ofrecemos iFraming de nuestras páginas de cobros. Si utilizas GoCardless Pro, ofrecemos un Flow JS
-          que te permite no tener ningún dato bancario en tu servidor.<br></br>
+          que te permite no tener ningún dato bancario en tu servidor.
         </p>
 
-        <h3 className='u-text-heading-light u-color-dark-gray u-margin-Vm u-text-s'>
-          ¿Se pueden hacer los pagos directamente a través de la API?
+        <h3 className='u-text-heading-light u-color-dark-gray u-margin-Txl u-margin-Bm u-text-s'>
+          <strong>
+            ¿Se pueden hacer los pagos directamente a través de la API?
+          </strong>
         </h3>
         <p className='para'>
           Solo una vez que la autorización se haya realizado. Hay que enviar a los clientes a nuestras páginas
           seguras de cobros para realizar la autorización inicial del Adeudo Directo.
         </p>
         <p className='para'>
-          Una vez que la autorización se haya realizado, se puede solicitar el cobro a través de la API.<br></br>
+          Una vez que la autorización se haya realizado, se puede solicitar el cobro a través de la API.
         </p>
 
-        <h3 className='u-text-heading-light u-color-dark-gray u-margin-Vm u-text-s'>
-          ¿Pueden los clientes pagar desde su móvil?
+        <h3 className='u-text-heading-light u-color-dark-gray u-margin-Txl u-margin-Bm u-text-s'>
+          <strong>
+            ¿Pueden los clientes pagar desde su móvil?
+          </strong>
         </h3>
         <p className='para'>
           Sí - GoCardless es compatible con móviles y tabletas. Nuestras páginas de cobros funcionarán con
