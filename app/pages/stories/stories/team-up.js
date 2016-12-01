6f1c8e5d4695ad6b3c14c4a9c80a289f12b92958
Samantha North <samanthajmnorth@gmail.com>
2016-12-01 12:06:00 +0000

    fixed company name
    amended to “Teamup”

--- app/pages/stories/stories/team-up.js
+++ app/pages/stories/stories/team-up.js
@@ -10,7 +10,7 @@ export default class StoriesTeamUp extends React.Component {
         title='We thought GoCardless was an incredible leap forward with a technological solution to a traditional method of payment.'
         author='Tim Green'
         position='Director'
-        company='TeamUp'
+        company='Teamup'
         benefits='TBC'
         bgimgUrl='url(/images/stories/story-images/hotpod-yoga/hero-hotpod-yoga.jpg)'
         sidebarLogoSrc='/images/stories/story-logos/teamup.png'
@@ -19,12 +19,12 @@ export default class StoriesTeamUp extends React.Component {
         sidebarPreviousProvider='TBC'>
         <div className='story-body'>
           <p className='para u-margin-Tn'>
-            <a href='https://goteamup.com/en-us/' className='u-link-color-p u-text-underline'>TeamUp</a> started as a consumer
+            <a href='https://goteamup.com/en-us/' className='u-link-color-p u-text-underline'>Teamup</a> started as a consumer
             facing business tool, which grew into one of the leading fitness industry focused business platforms. The company has
             grown through listening to customers and implementing feature upgrades that have made the customers more successful.
           </p>
           <p className='para'>
-            Five years later, thousands of fitness professionals now rely on TeamUp to make sure their customers have a friction-free
+            Five years later, thousands of fitness professionals now rely on Teamup to make sure their customers have a friction-free
             experience with their business.
           </p>
           <p className='para'>
@@ -37,7 +37,7 @@ export default class StoriesTeamUp extends React.Component {
             Discovering Direct Debit
           </h2>
           <p className='para'>
-            After experimenting with different methods, TeamUp discovered GoCardless and developed a custom integration around
+            After experimenting with different methods, Teamup discovered GoCardless and developed a custom integration around
             its Direct Debit payment software.
           </p>
           <p className='story-page__quote'>
@@ -48,17 +48,17 @@ export default class StoriesTeamUp extends React.Component {
             <img src='/images/stories/story-images/teamup/website@2x.jpg' alt='' />
           </div>
           <p className='para'>
-            According to TeamUp, the main benefits for any business of working with GoCardless is the incredibly stable and reliable
-            platform. TeamUp operate completely via the API, and have built additional functionality on top of the integration that
+            According to Teamup, the main benefits for any business of working with GoCardless is the incredibly stable and reliable
+            platform. Teamup operate completely via the API, and have built additional functionality on top of the integration that
             helps fitness customers.
           </p>
           <p className='para'>
             But any business can benefit from simply signing up and using the dashboard for recurring payments, without long term
-            commitment. In fact, TeamUp encourage new businesses to do this for starters, and then when they’re ready, upgrade to
-            TeamUp to add the extra functionality that the company offers.
+            commitment. In fact, Teamup encourage new businesses to do this for starters, and then when they’re ready, upgrade to
+            Teamup to add the extra functionality that the company offers.
           </p>
           <p className='story-page__quote'>
-            “The customer support has always been excellent at GoCardless and we have been able to trust the team to handle our
+            “The customer support at GoCardless has always been excellent and we've been able to trust the team to handle our
             customers in a personal and professional way, exactly the same as we do ourselves.”
           </p>
           <p className='para'>
@@ -70,28 +70,28 @@ export default class StoriesTeamUp extends React.Component {
             Integrating the GoCardless API
           </h2>
           <p className='para'>
-            Integrating GoCardless as a partner has been an easy ride for TeamUp.
+            Integrating GoCardless as a partner has been an easy ride for Teamup.
           </p>
           <p className='para'>
             “The key is the professional team behind the platform. The API has developed, and for any capable development team it’s
             simple to integrate it into existing payment flows. With a growing number of partners, it should be possible to find a
-            way to simply click and be immediately live – like with TeamUp for fitness and class based businesses.”
+            way to simply click and be immediately live – like with Teamup for fitness and class based businesses.”
           </p>
           <p className='story-page__quote'>
             “Customers love GoCardless, and have found it reliable and great value for the benefits they get from it.”
           </p>
           <p className='para'>
-            According to Tim, the fitness industry is micro-niching at some pace. This development is an exciting one for TeamUp
+            According to Tim, the fitness industry is micro-niching at some pace. This development is an exciting one for Teamup
             not just because the company helps small to medium businesses grow, but also because it’s full of fitness enthusiasts.
           </p>
           <p className='para'>
-            TeamUp also sees a huge rise in franchise businesses, so it has developed a special franchise management platform that
+            Teamup also sees a huge rise in franchise businesses, so it has developed a special franchise management platform that
             helps both franchisors and franchisees operate with the same ease as any of its customers.
           </p>
           <p className='para'>
-            This platform includes custom reporting, revenue share payment management and more. What’s more, TeamUp uses GoCardless
+            This platform includes custom reporting, revenue share payment management and more. What’s more, Teamup uses GoCardless
             as the mechanism, meaning that every size of business can also benefit from the same reliable payments as existing
-            TeamUp customers.
+            Teamup customers.
           </p>
         </div>
       </StoryPageNew>
