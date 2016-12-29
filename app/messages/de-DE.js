10af25398bd3212b005640fd25827580dd89803a
Joe Markevicius <joemarkevicius@gocardless.com>
2016-12-29 09:58:46 +0000

    Update price format

--- app/messages/de-DE.js
+++ app/messages/de-DE.js
@@ -8,12 +8,12 @@ export default {
   },
   phone_local: '030 568373022',
   pricing: {
-    cost_cap: '2€',
+    cost_cap: '2 €',
     description: 'Ziehen Sie Lastschriften online ein, und zahlen Sie nur 1% Gebühren, gedeckelt bei 2 €. Für große Unternehmen behalten wir es uns vor, Preisnachlässe anzubieten.',
-    pro_cost_per_transaction: '0,10€ — 0,60€',
+    pro_cost_per_transaction: '0,10 € — 0,60 €',
     per_transaction_amount_normal: '1 %',
-    pro_monthly_fee: '250€',
-    normal_capped_at_amount: '200€',
+    pro_monthly_fee: '250 €',
+    normal_capped_at_amount: '200 €',
     pro_number_of_monthly_payments: '> 500',
   },
 };
