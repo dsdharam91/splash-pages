bf22b41d10c957eae0bd561392e7a20c63cc707b
katie-mai <katie-mai@gocardless.com>
2016-12-30 13:42:01 +0000

    sales job ad
    PR for NBD ad - @jacobpgn @petehamilton please can someone check this?

---
+++ app/pages/about/jobs/positions/sales-development-representative-outbound.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import JobsPage from '../jobs-page';
+
+export default class SalesDevelopmentRepresentativeOutbound extends React.Component {
+  displayName = 'SalesDevelopmentRepresentativeOutbound'
+
+  render() {
+
+    return (
+      <JobsPage>
+            <h3 className='u-text-heading-light u-text-m u-color-dark-gray'>New Business Development Representative</h3>
+        <p className='para'>
+          Drive GoCardless’s global growth.
+        </p>
+        <p className='para'>
+          At GoCardless we are looking to scale from processing $1 billion a year to over $10 billion worldwide.
+          Our <a href='https://gocardless.com/blog/a-new-payment-network-for-the-internet/' target='_blank'> vision </a>
+          is to create a global payments network, making payments simpler on
+          the internet no matter what country you're in.
+        </p>
+        <p className='para'>
+          We're looking for bright, ambitious people to join our talented New
+          Business Development team and accelerate our growth in the UK and across Europe.
+        </p>
+        <h4 className='u-text-xs u-margin-Bxs u-margin-Tm'>The role</h4>
+        <p className='para'>
+         The Business Development team at GoCardless sits within our sales function
+         and combines deep product expertise, in-depth knowledge of the payments
+         industry, and a consultative approach to reach out to and work with
+         organisations operating across the UK and Europe.
+        </p>
+        <p className='para'>
+         Working closely with business and product leaders across GoCardless,
+         the NBD team work to uncover and define our best growth opportunities,
+         including new merchants, partners, verticals, and growth strategies. Recent
+         focus areas have involved close work in the utilities and telecoms sectors,
+         financial services and the subscription economy.
+        <p className='para'>
+          We generate new opportunities a little differently at GoCardless.
+          There will be no cold calling and instead a more consultative and targeted
+          approach is taken. The NBD team is focussed on developing large enterprise
+          opportunities for GoCardless so a deep understanding of our target markets
+          and prospects is essential to succeed.You'll implement outbound facing
+          activites to reach new merchants and be the first point of contact before
+          passing qualified opportunities on to our team of Enterprise Account Executives.
+        </p>
+        <p className='para'>
+          Training for the position will involve a 4 week structured program split
+          across both our NBD team as well as our Sales Development team. The program
+          will give you an in depth overview of the product, how we reach out to
+          enterprise-level merchants, and a detailed understanding of how we fit
+          into the wider payments industry.
+        </p>
+        </p>
+        <h4 className='u-text-xs u-margin-Bxs u-margin-Tm'>What we're looking for</h4>
+        <ul className='list u-margin-Tm'>
+          <li>
+            Previous experience within a sales environment or a top consultancy,
+            or recent graduates with commercial or technical backgrounds.
+          </li>
+          <li>
+            Strong analytical skills and the ability to use a wide range of resources
+            to come to clear conclusions and recommendations.
+          </li>
+          <li>
+            An ability to understand our API and build great relationships with
+             highly technical customers.
+          </li>
+          <li>
+            A passion for technology, the startup community, and supporting other
+            businesses as they grow.
+          </li>
+          <li>
+            The motivation and flexibility to work well in a rapidly changing, high-growth environment.
+          </li>
+          <li>
+            Superb verbal and written communication skills.
+          </li>
+        </ul>
+        <p className='para'>
+          Our team comes from a variety of different background and includes entrepreneurs, consultants and fresh graduates.
+          To help you, we've identified <a href='https://gocardless.com/blog/how-we-train-salespeople/' target='_blank'>five characteristics </a>
+          that are vital to be a successful member of our sales team.
+        </p>
+        <h4 className='u-text-xs u-margin-Bxs u-margin-Tm'>The perks</h4>
+        <ul className='list u-margin-Tm'>
+          <li>
+            Competitive salary, commission and options package.
+          </li>
+          <li>
+            Bi-weekly and monthly all team lunches and evenings out.
+          </li>
+          <li>
+            Stocked kitchen and unlimited (great) coffee!
+          </li>
+        </ul>
+        <p className='para'>
+          If you’re unsure, please apply &ndash; we’d love to hear from you.
+        </p>
+        <a href='https://boards.greenhouse.io/gocardless/jobs/162976' className='btn u-margin-Vs'>
+          Apply now
+        </a>
+      </JobsPage>
+    );
+  }
+}
